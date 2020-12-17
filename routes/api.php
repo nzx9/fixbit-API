8b71e8baae79a83fd0e189463892606b2546bdcc
navindu <navindudananga123@gmail.com>
2020-12-17 22:09:18 +0530

    users routes, auth routes and project routes addded

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProjectController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +15,11 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post("users/register", [UserController::class, "registerUser"]);
+Route::post("users/login", [UserController::class, "loginUser"]);
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware("auth:api")->group(function () {
+    Route::get("user", [UserController::class, "userDetails"]);
+    Route::resource("projects", ProjectController::class);
 });
+
