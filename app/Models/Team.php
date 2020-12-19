85a8948c8ebc115de794c1f056757594dfbe803f
navindu <navindudananga123@gmail.com>
2020-12-19 23:30:46 +0530

    new methods added

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -10,6 +10,46 @@ class Team extends Model
     use HasFactory;
 
     protected $casts = [
-        'leader_id' => 'integer'
+        'leader_id' => 'integer',
+        'is_active' => 'boolean'
     ];
+
+    /**
+     * Many to Many relationship defining
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function users(){
+        return $this->belongsToMany(User::class);
+    }
+
+    /**
+     * Create team member list table for given team
+     *
+     * @param int tid
+     * @return void
+     */
+    public function createIssueTable(int $tid)
+    {
+        Schema::create('team_' . $tid, function (Blueprint $table) {
+            $table->bigInteger('uid')->primary();
+            $table->string('name')->length(30);
+            $table->tinyInteger('is_leader')->nullable()->index();
+            $table->tinyInteger('is_available');
+            $table->string('role')->length('20')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Drop table table of given tid
+     *
+     * @param
+     * @return void
+     */
+
+    public function dropIssueTable(int $tid){
+        Schema::dropIfExists("team_".$tid);
+    }
+
 }
